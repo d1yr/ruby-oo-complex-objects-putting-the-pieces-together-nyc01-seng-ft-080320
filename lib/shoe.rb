86dd3d95248bcafeff07af7883a88006ee0628b8
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-08-05 02:20:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 # Make your shoe class here!
 class Shoe
-    attr_accessor :author, :page_count, :genre
-    attr_reader :title
+    #attr_accessor :author, :page_count, :genre
+    #attr_reader :title
     def initialize(brand)
       @brand = brand
     end
